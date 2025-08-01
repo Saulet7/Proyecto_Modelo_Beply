284d149f0e0469a2dc1f46df9f35757cac1361fe
Rachid <Rm109@alu.ua.es>
2025-08-01 12:14:35 +0200

    test.py

---
+++ almacen_tests/tests.py
@@ -0,0 +1,9 @@
+import subprocess
+
+subprocess.run(["python", "almacen_tests/test_almacenes.py"])
+#subprocess.run(["python", "almacen_tests/test_atributos.py"])
+subprocess.run(["python", "almacen_tests/test_fabricantes.py"])
+subprocess.run(["python", "almacen_tests/test_familias.py"])
+subprocess.run(["python", "almacen_tests/test_productos.py"])
+#subprocess.run(["python", "almacen_tests/test_stock.py"])
+subprocess.run(["python", "almacen_tests/test_transportistas.py"])
